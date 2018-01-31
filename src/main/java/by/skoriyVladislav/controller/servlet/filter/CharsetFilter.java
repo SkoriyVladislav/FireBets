7a43db8042f24ca7d821b2e25926e235f13dbcd7
SkoriyVladislav <Skoriy.97@gmail.com>
2018-01-31 19:03:44 +0300

    add charset filter

---
+++ src/main/java/by/skoriyVladislav/controller/servlet/filter/CharsetFilter.java
@@ -0,0 +1,37 @@
+package by.skoriyVladislav.controller.servlet.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+import java.io.IOException;
+
+@WebFilter(
+        filterName = "CharsetFilter",
+        servletNames = {"MainController"},
+        initParams = {@WebInitParam(name = "encoding", value = "UTF-8", description = "Encoding param")}
+)
+public class CharsetFilter implements Filter {
+    private String code;
+
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        code = filterConfig.getInitParameter("encoding");
+    }
+
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String codeRequest = request.getCharacterEncoding();
+        if ((codeRequest == null) || !code.equals(codeRequest)) {
+            request.setCharacterEncoding(code);
+            response.setCharacterEncoding(code);
+        }
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+        code = null;
+    }
+}
