b4f3258c8bfe1f9b8a4f270a04f66b6df13b1a4e
SkoriyVladislav <Skoriy.97@gmail.com>
2018-07-07 12:53:45 +0300

    Refactor, Fixed bags, Fix localization

--- src/main/java/by/skoriyVladislav/controller/servlet/MainController.java
+++ src/main/java/by/skoriyVladislav/controller/servlet/MainController.java
@@ -17,7 +17,7 @@ public class MainController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+
     }
 
     @Override
